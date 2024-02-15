982a9ec8ee771ef8a28edeb57b5d5430e03079f4
turnMeUpSon <milleryan2003@gmail.com>
2024-02-15 17:38:20 +0000

    Initial Commit

---
+++ IphoneSettingsApp/IphoneSettingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneSettingsAppApp.swift
+//  IphoneSettingsApp
+//
+//  Created by Ian Miller on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct IphoneSettingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
